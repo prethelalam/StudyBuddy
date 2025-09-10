23aad54564f546a18ae84589fcba3cd1e96d976a
Prethel Alam <prethelalam@Thels-MacBook-Pro.local>
2025-09-10 17:09:32 -0500

    added to routes to different tabs in router/index.js, added different views for each tab; began writing Navbar.vue in components folder

---
+++ apps/frontend/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- navbar set up -->
+  <nav class="navbar">
+    <div class="container">
+      <div class="logo">
+        <router-link to="/">Logo for the time being</router-link>
+      </div>
+      <div class="menu-icon" @click="toggleMenu">
+        <!-- insert code -->
+      </div>
+      <ul>
+
+        
+      </ul>
+    </div>
+  </nav>
+
+</template>
+
+<script setup>
+</script>
--- apps/frontend/src/router/index.js
+++ apps/frontend/src/router/index.js
@@ -1,22 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+
+// creating routes to the various tabs for nav bar
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/home',
+      name: 'Home',
+      component: () => import('../views/HomeView.vue'), // dynamic routing
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: () => import('../views/Dashboard.vue'), //this links to the actual page (dynamic path)
     },
+    {
+      path: '/quiz',
+      name: 'Quiz',
+      component: () => import('../views/Quiz.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: () => import('../views/Profile.vue'),
+    }
   ],
 })
 
