f2e014aa18976bb1f124053559781e3bdb6538bc
Prethel Alam <prethelalam@Thels-MacBook-Pro.local>
2025-09-11 08:46:48 -0500

    added to Navbar.vue file, created toggle function

--- apps/frontend/src/components/Navbar.vue
+++ apps/frontend/src/components/Navbar.vue
@@ -5,12 +5,14 @@
       <div class="logo">
         <router-link to="/">Logo for the time being</router-link>
       </div>
-      <div class="menu-icon" @click="toggleMenu">
+      <div class="menu-icon" @click="toggle">
         <!-- insert code -->
       </div>
-      <ul>
+      <!-- create an unordered list with data binding. true means it always applys the css; isOpen is a variable  -->
+      <ul :class="{ 'nav-links': true, 'open': isOpen }">
+        <li><router-link to="/home" @click="toggle">Home</router-link></li>
 
-        
+
       </ul>
     </div>
   </nav>
@@ -18,4 +20,14 @@
 </template>
 
 <script setup>
+
+// function to toggle on and off
+function toggle() {
+  if (this.isOpen == true) {
+    this.isOpen = false;
+  }
+  else if (this.isOpen == false) {
+    this.isOpen = true;
+  }
+};
 </script>
