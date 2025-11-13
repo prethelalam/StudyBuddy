fa07a274d6a3f7d8f620d3abb53fd1918d23097b
Prethel Alam <prethelalam@Thels-MacBook-Pro.local>
2025-11-13 13:48:20 -0600

    changed hero image on Home.vue

--- apps/frontend/src/views/Home.vue
+++ apps/frontend/src/views/Home.vue
@@ -11,7 +11,7 @@
 
     <div
       class="hero min-h-screen"
-      style="background-image: url('/images/logo.png')" >
+      style="background-image: url('/images/StudyBuddyLogo.png')" >
   <div class="hero-overlay"></div>
   <div class="hero-content text-neutral-content text-center">
     <div class="max-w-md">
